e028d9587a3d73591a45a0d83dea43199661459f
akanehl <akanehl@ucsc.edu>
2019-10-19 11:13:15 -0700

    Dad movement added
    Its gross, but it works. Map also needs  to be tweaked

---
+++ CMPM170-proto1/Assets/dadMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dadMovement : MonoBehaviour
+{
+    public Rigidbody2D dadBod;
+    public float moveSpeed;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        dadBod.MovePosition(dadBod.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
