2f146209115355c97fe6a63b94d521b34692724f
Erk <acarerk@gmail.com>
2019-10-25 12:42:18 -0700

    baby and dad interaction scripts

---
+++ CMPM170-proto1/Assets/babyInteraction.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class babyInteraction : MonoBehaviour
+{
+    //variables to keep track of interactable object in range
+    bool inRange = false;
+    GameObject interactObj;
+    public GameObject indicatorPrefab;
+    //boolean to keep track of charge
+    bool isCharged = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactObj = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void setFire()
+    {
+
+    }
+
+    //function to be called when player attempts to interact
+    void interact()
+    {
+        if (inRange)
+        {
+            if (interactObj.tag == "Outlet")
+            {
+                grabCharge();
+            }
+            if (interactObj.tag == "Chair" && isCharged)
+            {
+                setFire();
+            }
+        }
+    }
+
+    void grabCharge()
+    {
+        if (!isCharged)
+        {
+            isCharged = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        interactObj = col.gameObject;
+        inRange = true;
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        interactObj = null;
+        inRange = false;
+    }
+    public void sprayed()
+    {
+        if (isCharged)
+        {
+            Debug.Log("extinguished");
+            isCharged = false;
+        }
+    }
+    public void checkInput()
+    {
+        if (inRange)
+        {
+            if (this.gameObject.name == "Baby_1" && Input.GetKeyDown(KeyCode.E))
+            {
+                interact();
+            }
+            else if (this.gameObject.name == "Baby_2" && Input.GetKeyDown(KeyCode.Y))
+            {
+                interact();
+            }
+            else if (this.gameObject.name == "Baby_1" && Input.GetKeyDown(KeyCode.O))
+            {
+                interact();
+            }
+        }
+    }
+}
---
+++ CMPM170-proto1/Assets/dadInteraction.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dadInteraction : MonoBehaviour
+{
+    //variables to keep track of interactable object in range
+    bool inRange = false;
+    GameObject interactObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactObj = null;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            interact();
+        }
+    }
+    //function executed when the player is in range and attempts to interact with an object
+    void interact()
+    {
+        Debug.Log("interacting");
+        if (inRange)
+        {
+            if (interactObj.tag == "Stove")
+            {
+                cook();
+            }
+            if (interactObj.tag == "Baby")
+            {
+                spray();
+            }
+        }
+    }
+    //function to trigger the cooking event
+    void cook()
+    {
+        Debug.Log("cooking");
+        //call function in game state management
+    }
+    //function to spray baby
+    void spray()
+    {
+        interactObj.GetComponent<babyInteraction>().sprayed();
+        //call function in baby interaction
+    }
+    //trigger functions to keep track of when the player is in range of an interactable object
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        interactObj = col.gameObject;
+        inRange = true;
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        interactObj = null;
+        inRange = false;
+    }
+}
